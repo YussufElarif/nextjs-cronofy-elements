f4cc0338b1d6a26cc832e78f51f9433e53f7e5c7
yussuf elarif <yussuf@cronofy.com>
2022-01-26 09:24:14 +0000

    fix indent

--- components/CalendarSyncWrapper.js
+++ components/CalendarSyncWrapper.js
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import * as CronofyElements from "cronofy-elements";
 
 export const CalendarSyncWrapper = ({ options }) => {
-    const [element, setElement] = useState(null);
-  
-    useEffect(() => {
-      if (!element) {
-        setElement(CronofyElements.CalendarSync(options));
-      }
-    }, []);
-  
-    useEffect(() => {
-      if (element) {
-        element.update(options);
-      }
-    }, [options]);
-  
-    return <div id="cronofy-calendar-sync" />;
-  };
+  const [element, setElement] = useState(null);
+
+  useEffect(() => {
+    if (!element) {
+      setElement(CronofyElements.CalendarSync(options));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (element) {
+      element.update(options);
+    }
+  }, [options]);
+
+  return <div id="cronofy-calendar-sync" />;
+};
   
